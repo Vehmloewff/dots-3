ce63f16b8d596f1ff07c4ea015e82453bee7100d
Elisarmor <ejsisgreat@gmail.com>
2019-05-24 09:17:05 -0500

    Fix a couple bugs and break the api

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # dots-3
+Slim down a string with ...
+
+## Usage
+Install the package...
+```
+$ npm i dots-3 --save
+```
+...then...
+```
+const dots3 = require('dots-3'); // or import dots3 from 'dots-3'
+
+dots3('The quick brown fox jumped over the lazy dog.', 9);
+// -> The quic…
+
+dots3('The quick brown fox jumped over the lazy dog.', 3000);
+// -> The quick brown fox jumped over the lazy dog.
+
+dots3('What? Is that a monkey for a pet?', 5);
+// -> What? …
--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = (text, chars) => {
 	let dots = String(text).slice(0, Number(chars - 3)).trim();
 	let lastChar = dots.charAt(dots.length - 1)
 	if (dots.length + 5 < Number(chars)) return dots;
-	else if (lastMark(dots)) return dots + ' ...'
-	else return dots + '...'
+	else if (lastMark(dots)) return dots + ' …'
+	else return dots + '…'
 }
 function lastMark(text) {
 	if (text == '.' || text == ',' || text == ';' || text == '!' || text == '?') return true;
